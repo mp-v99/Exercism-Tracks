89ce10b57be474b357badd96f4260b95b1065dfd
mpv <193361506+mp-v99@users.noreply.github.com>
2025-08-27 00:33:39 -0600

    feat: solve poetry exercise

--- exercism/javascript/poetry-club-door-policy/door-policy.js
+++ exercism/javascript/poetry-club-door-policy/door-policy.js
@@ -27,6 +27,9 @@
  * @returns {string}
  */
 
+  export function frontDoorResponse(line) {
+    return line[0]
+  }
 
 /**
  * Format the password for the front-door, given the response
@@ -36,6 +39,9 @@
  * @returns {string} the front door password
  */
 
+export function frontDoorPassword(word) {
+  return frontDoorResponse(word).toUpperCase() + word.slice(1).toLowerCase();
+}
 
 /**
  * Respond with the correct character, given the line of the
@@ -45,7 +51,9 @@
  * @returns {string}
  */
 
-
+export function backDoorResponse(line) {
+  return line.trimEnd().slice(-1)
+}
 /**
  * Format the password for the back door, given the response
  * letters.
@@ -53,3 +61,8 @@
  * @param {string} word the letters you responded with before
  * @returns {string} the back door password
  */
+
+export function backDoorPassword(word) {
+  return `${frontDoorPassword(word)}, please`
+
+}
